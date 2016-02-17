bb19455c0ae66579951fc8193f99bb4d562f47ca
Xavi Giménez Sánchez <javiergimenezsanchez@gmail.com>
2016-02-17 12:10:37 +0000

    Added jqury.nav.js

--- public/js/enplater.js
+++ public/js/enplater.js
@@ -1,7 +1,14 @@
-$('.mainHeader').vegas({
-    slides: [
-        { src: 'img/foto-6.jpg' },
-        { src: 'img/foto-4.jpg' },
-        { src: 'img/foto-3.jpg' }
-    ]
-});
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    $('.mainHeader').vegas({
+        slides: [
+            { src: 'img/foto-6.jpg' },
+            { src: 'img/foto-4.jpg' },
+            { src: 'img/foto-3.jpg' }
+        ]
+    });
+    	$('#nav-scroll').onePageNav({offset: 200});
+  });
+  
+  //{offset: $('.fixe').outerHeight()+50}
